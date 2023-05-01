0b94f8d29d24d84587907d3425ff9daac87296e6
IVaN Ivanovi4 <eng.vano@yandex.ru>
2023-05-01 20:48:32 +0300

    Add slider

--- src/components/blocks/main/Main.jsx
+++ src/components/blocks/main/Main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.css";
 import imgMain from "../../../assets/Polygon.png";
+import Button from "../../ui/button/Button";
 
 export default function Main() {
   return (
@@ -17,9 +18,7 @@ export default function Main() {
         xmlns="http://www.w3.org/2000/svg"
         className="svg__bg"
       >
-        <path
-          d="M573.067 3.63485C578.205 -1.65774 587.109 0.244538 589.636 7.17473L869.84 775.594C872.533 782.979 866.044 790.441 858.356 788.799L8.41648 607.222C0.728696 605.579 -2.14513 596.117 3.33081 590.476L573.067 3.63485Z"
-        />
+        <path d="M573.067 3.63485C578.205 -1.65774 587.109 0.244538 589.636 7.17473L869.84 775.594C872.533 782.979 866.044 790.441 858.356 788.799L8.41648 607.222C0.728696 605.579 -2.14513 596.117 3.33081 590.476L573.067 3.63485Z" />
       </svg>
 
       <h3>frontend developer</h3>
@@ -38,7 +37,6 @@ export default function Main() {
           stroke-width="2"
         />
       </svg>
-      
     </div>
   );
 }
